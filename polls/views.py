20b70f003cba970a2f22ed338a8055ced586fa8c
Manuel Lopez <manuelfx117@gmail.com>
2020-08-07 22:43:42 -0500

    primer crud

--- polls/views.py
+++ polls/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from polls.models import Users
-
-
+import os
 import json
+import jwt
+import bcrypt
+import datetime
+
 def  index(request):
     return HttpResponse("Hello world, estas en poll index")
 
@@ -16,14 +19,16 @@ def putPoll(request):
         total = Users.objects.all()
         print('total: ',total)
         print(total[0])
-        return JsonResponse({"Hola":'DELETE'})
+        return JsonResponse({"Hola":'GET'})
 
     if request.method == "POST":
-        data = json.loads(request.body)
-        user = Users(nombre = data['nombre'],correo = data['correo'], password = data['password'])
-        user.save()
 
-        return JsonResponse({"Hola":'POST','data':data,'id':user.id})
+        data = json.loads(request.body)
+        hashed = bcrypt.hashpw(data['password'].encode('utf8'), bcrypt.gensalt())
+        user = Users(nombre = data['nombre'],correo = data['correo'], password = hashed)
+        user.save()
+        encoded_jwt = jwt.encode({'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=30), 'user':user.correo},(os.environ.get('SECRET_JWT') or 'secret'), algorithm='HS256')
+        return JsonResponse({"Hola":'POST','encoded_jwt':encoded_jwt.decode('ascii')})
 
     if request.method == "PUT":
         data = json.loads(request.body)
